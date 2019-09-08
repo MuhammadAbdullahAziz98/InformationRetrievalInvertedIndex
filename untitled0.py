ff46ce3c7859bd0102ced4e931e184d827828a9b
abdullah <abdullahaziz1998@gmail.com>
2019-09-09 03:56:20 +0500

    Added arguments functionality

--- untitled0.py
+++ untitled0.py
@@ -6,15 +6,20 @@ Created on Sun Sep  8 21:17:32 2019
 """
 
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
-
+#imports
 import glob, os
-os.chdir(r"C:\Users\lenovo\Downloads\corpus\corpus\corpus")
-for file in glob.glob("clueweb12-0000tw-14-17002.txt"):
-    #print(file)
-    fp = open(file,'r',encoding=" Latin-1 ")
-    print(fp.read())
+import sys
+
+
+#getting arguments:
+
+filename = sys.argv[1]
+if os.path.exists(filename):
+    os.chdir(filename)
+    for file in glob.glob("clueweb12-0000tw-14-17002.txt"):
+        #print(file)
+        fp = open(file,'r',encoding=" Latin-1 ")
+        print(fp.read())
+else:
+    print("Folder doesn't exist, try entering the complete path")
