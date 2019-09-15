3fb7d2d640c0ad29f2b2281ae794b488efb44e61
abdullah <abdullahaziz1998@gmail.com>
2019-09-15 17:48:25 +0500

    Added lowercasing and stopwording

--- tokenizer.py
+++ tokenizer.py
@@ -16,10 +16,18 @@ import re
 #getting arguments:
 
 filename = sys.argv[1]
-
+stopwords = dict()
+stopwordsFile = r"C:\Users\lenovo\Downloads\stoplist.txt"   #change  apth for stopwords list
+if os.path.exists(stopwordsFile):
+    swfp = open(stopwordsFile,'r')
+    content = swfp.readlines()
+    for x in content:
+        stopwords[x.strip()] = x.strip()  
 if os.path.exists(filename):
     os.chdir(filename)
-    for file in glob.glob("*"):
+    i = 0
+    worddict =  dict()
+    for file in glob.glob("clueweb12-0000tw-14-17002.txt"):
         #print(file)
         fp = open(file,'r',encoding=" Latin-1 ")
         #print(fp.read())
@@ -32,13 +40,21 @@ if os.path.exists(filename):
             for script in soup(["script", "style"]):                   
                 script.decompose()
             docWords = list(el.stripped_strings)
-            print(docWords)
-            wordlist =  list()
-            pattern = '[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+|[]!"$%&\'()*+,./:;=#@?[\\^_`{|}~-]+'
+           # print(docWords)
+            wordlist = list()
+            pattern = '[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+'
             for w in docWords:
                 wordlist.append(re.findall(pattern,w))
-           # print(wordlist)            
+            vocab = [item for sublist in wordlist for item in sublist]
+            for w in stopwords:
+                worddict.pop(w, None)
+            for w in vocab:
+                if w.lower() not in worddict and w.lower() not in stopwords:
+                    worddict[w.lower()] = i
+                    i = i+1
+
+                #print(re.findall(pattern,w))
+            print(worddict)            
             print("FILE ENDS HEREE!!!!!!!!!!!!!!!!!!")
-        
 else:
     print("Folder doesn't exist, try entering the complete path")
