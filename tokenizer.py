e7a6d3bed025c729a7ea3b24458a55c34bb5a95b
abdullah <abdullahaziz1998@gmail.com>
2019-09-16 12:49:42 +0500

    Added docids

--- tokenizer.py
+++ tokenizer.py
@@ -26,12 +26,17 @@ if os.path.exists(stopwordsFile):
         stopwords[x.strip()] = x.strip()  
 if os.path.exists(filename):
     os.chdir(filename)
-    i = 0
+    termid = 0
+    docid=  0
     worddict =  dict()
     p_stemmer = PorterStemmer()
-    for file in glob.glob("clueweb12-0000tw-14-17002.txt"):
+    for file in glob.glob("*.txt"):
         #print(file)
         fp = open(file,'r',encoding=" Latin-1 ")
+        fd = open(r"C:\Users\lenovo\Documents\irAssignment1\docids.txt", "a",encoding=" utf-8 ")
+        fd.write(file + "\\t" + str(docid)+"\n")
+        fd.close()
+        docid = docid+1
         #print(fp.read())
         soup = BeautifulSoup(fp, 'html.parser')
         el = soup.find('body')
@@ -44,7 +49,7 @@ if os.path.exists(filename):
             docWords = list(el.stripped_strings)
            # print(docWords)
             wordlist = list()
-            pattern = '[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+'
+            pattern = pattern = r'[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|\w+-\w+|\w+.\w+|\w+_\w+'
             for w in docWords:
                 wordlist.append(re.findall(pattern,w))
             vocab = [item for sublist in wordlist for item in sublist]
@@ -53,10 +58,14 @@ if os.path.exists(filename):
                 w = w.lower()
                 w = p_stemmer.stem(w)
                 if w not in worddict and w not in stopwords:
-                    worddict[w] = i
-                    i = i+1
-
-            print(worddict)            
-            print("FILE ENDS HEREE!!!!!!!!!!!!!!!!!!")
+                    worddict[w] = termid
+                    termid = termid+1
+            #print(worddict)            
+            #print("FILE ENDS HEREE!!!!!!!!!!!!!!!!!!")
+    f = open(r"C:\Users\lenovo\Documents\irAssignment1\termids.txt", "w",encoding=" utf-8 ")
+    for w in worddict:
+        f.write(w + "\\t" + str(worddict[w])+"\n")
+    f.close()
+    
 else:
     print("Folder doesn't exist, try entering the complete path")
