0b1802ecf716167dc235504edaae6c263196036a
abdullah <abdullahaziz1998@gmail.com>
2019-09-17 02:09:55 +0500

    Added delta encoding hashmap inverted index

--- tokenizer.py
+++ tokenizer.py
@@ -13,7 +13,7 @@ import sys
 from bs4 import BeautifulSoup,Comment
 import re
 from nltk.stem.porter import PorterStemmer
-
+from operator import itemgetter
 class TokenHashmap:
     numberOfOccurences = 0
     totalNoOfDocs = 0
@@ -67,8 +67,7 @@ if os.path.exists(filename):
             for w in docWords:
                 wordlist.append(re.findall(pattern,w))
             vocab = [item for sublist in wordlist for item in sublist]
-            
-            for w in vocab:
+            for i, w in enumerate(vocab):
                 w = w.lower()
                 w = p_stemmer.stem(w)
                 if w not in worddict and w not in stopwords:
@@ -77,7 +76,7 @@ if os.path.exists(filename):
                     wordinfoHash[termid].docs = list()
                     wordinfoHash[termid].pos = list()
                     wordinfoHash[termid].docs.append(docid)
-                    wordinfoHash[termid].pos.append(fp.tell())
+                    wordinfoHash[termid].pos.append(i)
                     wordinfoHash[termid].numberOfOccurences = wordinfoHash[termid].numberOfOccurences+1
                     wordinfoHash[termid].totalNoOfDocs = wordinfoHash[termid].totalNoOfDocs+1
                     #without hashmap:
@@ -86,7 +85,7 @@ if os.path.exists(filename):
                     wordinfoWithoutHash[termid].docs = list()
                     wordinfoWithoutHash[termid].pos = list()
                     wordinfoWithoutHash[termid].docs.append(docid)
-                    wordinfoWithoutHash[termid].pos.append(fp.tell())
+                    wordinfoWithoutHash[termid].pos.append(i)
                     wordinfoWithoutHash[termid].numberOfOccurences = wordinfoWithoutHash[termid].numberOfOccurences+1
                     wordinfoWithoutHash[termid].totalNoOfDocs = wordinfoWithoutHash[termid].totalNoOfDocs+1
 
@@ -95,7 +94,7 @@ if os.path.exists(filename):
                     ids = worddict.get(w,None)
                     if ids:
                         print(ids)
-                        wordinfoHash[ids].pos.append(fp.tell())
+                        wordinfoHash[ids].pos.append(i)
                         wordinfoHash[ids].docs.append(docid)
                         wordinfoHash[ids].numberOfOccurences = wordinfoHash[ids].numberOfOccurences+1
                         wordinfoHash[ids].totalNoOfDocs = wordinfoHash[ids].totalNoOfDocs+1
@@ -106,14 +105,36 @@ if os.path.exists(filename):
                             idW = ind
                     if idW:
                         wordinfoWithoutHash[idW].docs.append(docid)
-                        wordinfoWithoutHash[idW].pos.append(fp.tell())
+                        wordinfoWithoutHash[idW].pos.append(i)
                         wordinfoWithoutHash[idW].numberOfOccurences = wordinfoWithoutHash[idW].numberOfOccurences+1
                         wordinfoWithoutHash[idW].totalNoOfDocs = wordinfoWithoutHash[idW].totalNoOfDocs+1
-
+    #sorting
+    idW=0                
+    for idW in range(0,len(wordinfoWithoutHash)):
+        [list(x) for x in zip(*sorted(zip(wordinfoWithoutHash[idW].docs, wordinfoWithoutHash[idW].pos), key=itemgetter(0)))]
+    ids=0                
+    for idW in range(0,len(wordinfoWithoutHash)):
+        [list(x) for x in zip(*sorted(zip(wordinfoHash[ids].docs, wordinfoHash[ids].pos), key=itemgetter(0)))]
+    #termids.txt
     f = open(r"C:\Users\lenovo\Documents\irAssignment1\termids.txt", "w",encoding=" utf-8 ")
     for w in worddict:
         f.write(w + "\\t" + str(worddict[w])+"\n")
     f.close()
+    
+    # delta encoding:
+    print("ENCODING STARTS:")
+    for w in wordinfoHash:
+        for p in range(len(wordinfoHash[w].docs)-1,0,-1):
+            wordinfoHash[w].docs[p] = wordinfoHash[w].docs[p] -wordinfoHash[w].docs[p-1]
+    for w in wordinfoHash:
+        for p in range(len(wordinfoHash[w].pos)-1,0,-1):
+            if wordinfoHash[w].pos[p] -wordinfoHash[w].pos[p-1]>=0:
+                wordinfoHash[w].pos[p] = wordinfoHash[w].pos[p] -wordinfoHash[w].pos[p-1]
+    
+    #for w in range(0,len(wordinfoWithoutHash)):
+        #for p in range(0,len(wordinfoWithoutHash[w].pos)):
+    
+    #inverted index with hashmap 
     f = open(r"C:\Users\lenovo\Documents\irAssignment1\termindex_hashmap.txt", "w",encoding=" utf-8 ")
     for w in wordinfoHash:
         f.write(str(w) + " " + str(wordinfoHash[w].numberOfOccurences) +" "+str(wordinfoHash[w].totalNoOfDocs) +" ")
@@ -121,6 +142,7 @@ if os.path.exists(filename):
             f.write(str(wordinfoHash[w].docs[p]) +","+str(wordinfoHash[w].pos[p]) + " ")
         f.write("\n")            
     f.close()
+    #inverted index without hashmap 
     f = open(r"C:\Users\lenovo\Documents\irAssignment1\termindex_withouthashmap.txt", "w",encoding=" utf-8 ")
     for w in range(0,len(wordinfoWithoutHash)):
         f.write(str(wordinfoWithoutHash[w].termid) + " " + str(wordinfoWithoutHash[w].numberOfOccurences) +" "+str(wordinfoWithoutHash[w].totalNoOfDocs) +" ")
