d76cce1f171d97632028231a71b94b3f11187695
abdullah <abdullahaziz1998@gmail.com>
2019-09-20 01:47:04 +0500

    tokenizer file has inverted index with hashmap only

--- tokenizer.py
+++ tokenizer.py
@@ -54,6 +54,10 @@ if os.path.exists(filename):
     wordinfoHash = dict()
     wordinfoWithoutHash = list()
     p_stemmer = PorterStemmer()
+    fd = open(r"C:\Users\lenovo\Documents\irAssignment1\docids.txt", "w",encoding=" utf-8 ")
+    fd.write("")
+    fd.close()
+
     for file in glob.glob("*"):
         try:
             fp = open(file,"r",encoding="Latin-1")
@@ -73,13 +77,14 @@ if os.path.exists(filename):
                 element.extract()
             for script in soup(["script", "style"]):                   
                 script.extract()
-            docWords = list(el.stripped_strings)
-            vocab = list()
-            pattern = r'[A-Z]+\-[A-Z]+|[a-z]+\-[a-z]+|[A-Z]+\'[A-Z]+|[a-z]+\'[a-z]+|[A-Z][a-z]+|[a-z]+|[A-Z]+|[0-9]+'
+            docWords = soup.body.get_text()#list(el.stripped_strings)
+#            vocab = list()
+            pattern = r'[0-9]+\.[0-9]+|[A-Z]+\.[A-Z]+|[a-z]+\.[a-z]+|[A-Z]+\'[A-Z]+|[a-z]+\'[a-z]+|[A-Z][a-z]+|[a-z]+|[A-Z]+|[0-9]+'
             
-            for w in docWords:
-                vocab.append(re.findall(pattern,w))
-            vocab = [item for sublist in vocab for item in sublist]
+            #for w in docWords:
+            vocab = re.findall(pattern,docWords)
+            print(vocab)
+            #vocab = [item for sublist in vocab for item in sublist]
             for i, w in enumerate(vocab):
                 w = w.lower()
                 w = p_stemmer.stem(w)
@@ -93,12 +98,6 @@ if os.path.exists(filename):
                     wordinfoHash[termid].pos.append(i)
                     wordinfoHash[termid].numberOfOccurences = wordinfoHash[termid].numberOfOccurences+1
                     wordinfoHash[termid].totalNoOfDocs = wordinfoHash[termid].totalNoOfDocs+1    
-                    #without hashmap
-                    t = TokenWithoutHashmap()
-                    t.docid = docid
-                    t.termid = termid
-                    t.pos = i
-                    wordinfoWithoutHash.append(t)
                     termid = termid+1
                 else:
                     ids = worddict.get(w,None)
@@ -107,47 +106,8 @@ if os.path.exists(filename):
                         wordinfoHash[ids].docs.append(docid)
                         wordinfoHash[ids].numberOfOccurences = wordinfoHash[ids].numberOfOccurences+1
                         wordinfoHash[ids].totalNoOfDocs = wordinfoHash[ids].totalNoOfDocs+1
-                        t = TokenWithoutHashmap()
-                        t.docid = docid
-                        t.termid = ids
-                        t.pos = i
-                        wordinfoWithoutHash.append(t)
             fp.close()
 
-    #sorting for hashmap
-    idW=0                
-    
-    #without hashmap:
-    #sort on termid:
-    wordinfoWithoutHash.sort(key = custom_sort)
-    #merging on same term id: 
-    wordinfo = list()
-    oldtid = 0
-    newtid = 0
-    olddocid = 0
-    newdocid = 0
-    t= Token()
-    for w in range(0,len(wordinfoWithoutHash)):
-        newtid = wordinfoWithoutHash[w].termid
-        if newtid == oldtid and newtid!=0:
-            t.numberOfOccurences = t.numberOfOccurences+1
-            t.docs.append(wordinfoWithoutHash[w].docid)
-            t.pos.append(wordinfoWithoutHash[w].pos)
-            if olddocid == newdocid:
-                t.totalNoOfDocs = t.totalNoOfDocs+1
-            else:
-                olddocid = newdocid
-        else:    
-            oldtid = newtid
-            t = Token()
-            t.termid = newtid
-            t.totalNoOfDocs = t.totalNoOfDocs+1
-            t.numberOfOccurences = t.numberOfOccurences+1
-            t.docs = list()
-            t.pos = list()
-            t.docs.append(wordinfoWithoutHash[w].docid)
-            t.pos.append(wordinfoWithoutHash[w].pos)
-            wordinfo.append(t)
     #termids.txt
     f = open(r"C:\Users\lenovo\Documents\irAssignment1\termids.txt", "w",encoding=" utf-8 ")
     for w in worddict:
@@ -163,33 +123,20 @@ if os.path.exists(filename):
         for p in range(len(wordinfoHash[w].pos)-1,0,-1):
             if wordinfoHash[w].pos[p] -wordinfoHash[w].pos[p-1]>=0:
                 wordinfoHash[w].pos[p] = wordinfoHash[w].pos[p] - wordinfoHash[w].pos[p-1]
-    
-    for w in range(0,len(wordinfo)):
-        for p in range(len(wordinfo[w].docs)-1,0,-1):
-            wordinfo[w].docs[p] = wordinfo[w].docs[p] - wordinfo[w].docs[p-1]
-    for w in range(0,len(wordinfo)):
-        for p in range(len(wordinfo[w].docs)-1,0,-1):
-            if wordinfo[w].pos[p] -wordinfo[w].pos[p-1]>=0:
-                wordinfo[w].pos[p] = wordinfo[w].pos[p] - wordinfo[w].pos[p-1]
-    
+        
     #inverted index with hashmap 
     f = open(r"C:\Users\lenovo\Documents\irAssignment1\termindex_hashmap.txt", "w",encoding=" utf-8 ")
+    fi = open(r"C:\Users\lenovo\Documents\irAssignment1\terminfo.txt", "w",encoding=" utf-8 ")
     for w in wordinfoHash:
+        fi.write(str(w)+"\t" +str(f.tell()))
+        fi.write("\n")            
         f.write(str(w) + " " + str(wordinfoHash[w].numberOfOccurences) +" "+str(wordinfoHash[w].totalNoOfDocs) +" ")
         for p in range(0,len(wordinfoHash[w].pos)):
             f.write(str(wordinfoHash[w].docs[p]) +","+str(wordinfoHash[w].pos[p]) + " ")
-        f.write("\n")            
+        f.write("\n\n\n")            
     f.close()
+    fi.close()
     
-    #inverted index without hashmap 
-    f = open(r"C:\Users\lenovo\Documents\irAssignment1\termindex_withouthashmap.txt", "w",encoding=" utf-8 ")
-    for w in range(0,len(wordinfo)):
-        f.write(str(wordinfo[w].termid) + " " + str(wordinfo[w].numberOfOccurences) +" "+str(wordinfo[w].totalNoOfDocs) +" ")
-        for p in range(0,len(wordinfo[w].pos)):
-            f.write(str(wordinfo[w].docs[p]) +","+str(wordinfo[w].pos[p]) + " ")
-        f.write("\n")            
-    
-    f.close()
     
 else:
     print("Folder doesn't exist, try entering the complete path")
